dace191fbb67522f644d5cfb9f23728f72b8cfb3
NetAcad DEVASC <changeme@example.com>
2021-03-02 21:41:30 +0100

    Step1 and Step2 completed

--- pdfCreator.py
+++ pdfCreator.py
@@ -10,19 +10,20 @@ name = "Ex1" #HA D'AGAFAR EL NOM DEL FITXER
 pdf.add_page() #Adds a new page
 pdf.set_margins(26.1, 23.3, 26.1) #Sets the margins
 pdf.set_font('Arial', '', 30) #Sets the font and its style and size
-pdf.set_line_width(4) #Sets the width of the line
+pdf.set_line_width(5) #Sets the width of the line
 pdf.set_draw_color(248, 172, 4) #Sets the color of the line (light orange)
 pdf.line(26.1, 23.3, 26.1, 245) #Sets the position of the line
 
 pdf.image('/home/devasc/labs/devnet-src/python/lab1-python-ac-ob/tecnocampus-logo.jpg', x=130, y=23.3, w=70) #Adds an image (logo)
 pdf.set_xy(40, 70) #Sets in which x and y of the page starts writting
-title1 = "Informació infraestructura"
-title2 = "de Xarxa TecnoCampus"
+title = "Informació infraestructura de Xarxa TecnoCampus "
+titlept1 = "Informació infraestructura"
+titlept2 = "de Xarxa TecnoCampus"
 ex = "(" + name + ")"
-pdf.cell(w=0, h=0, txt=title1, border=0, ln=2, align='L') #Writes the 1st lane of the title
+pdf.cell(w=0, h=0, txt=titlept1, border=0, ln=2, align='L') #Writes the 1st lane of the title
 pdf.ln(12)
 pdf.set_x(40)
-pdf.cell(w=0, h=0, txt=title2, border=0, ln=2, align='L') #Writes the 2nd lane of the title
+pdf.cell(w=0, h=0, txt=titlept2, border=0, ln=2, align='L') #Writes the 2nd lane of the title
 pdf.ln(12)
 pdf.set_x(40)
 pdf.cell(w=0, h=0, txt=ex, border=0, ln=2, align='L') #Writes the name of the Ex
@@ -33,6 +34,25 @@ locale.setlocale(locale.LC_ALL, 'ca_ES.UTF-8') #Sets locale to ca_ES to have the
 dt = datetime.date.today()
 date = dt.strftime("%B %Y")
 pdf.cell(w=0, h=0, txt=date.replace("de ", "").capitalize(), border=0, ln=2, align='L') #Writes the date
+
+#Header
+def header():
+    pdf.image('/home/devasc/labs/devnet-src/python/lab1-python-ac-ob/tecnocampus-logo-light.jpg', x=140, y=5, w=40) #Adds an image (logo)
+    pdf.set_xy(26.1, 10)
+    pdf.set_font('Arial', '', 11) #Sets the font and its style and size
+    pdf.set_text_color(110, 110, 110) #Sets the color of the text (grey)
+    pdf.cell(w=0, h=0, txt=title + ex, border=0, ln=0, align='L') #Writes the title
+#Footer
+def footer():
+    pdf.set_xy(26.1, 276)
+    pdf.set_font('Arial', '', 11) #Sets the font and its style and size
+    pdf.set_text_color(110, 110, 110) #Sets the color of the text (grey)
+    page = str(pdf.page_no()) #Gets the page number
+    pdf.cell(w=0, h=0, txt=page, border=0, ln=0, align='L') #Adds a page number
+
+pdf.add_page() #Adds a new page
+header()
+footer()
 
 name = name + ".pdf"
 filename = "/home/devasc/labs/devnet-src/python/lab1-python-ac-ob/" + name
