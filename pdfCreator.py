1688e24aa7a71643aad75c4703c0cb04272bcf66
NetAcad DEVASC <changeme@example.com>
2021-03-02 20:12:33 +0100

    Portada feta (canviar data en a esp)

--- pdfCreator.py
+++ pdfCreator.py
@@ -1,4 +1,5 @@
 from fpdf import FPDF
+import datetime
 import os
 
 pdf = FPDF('P', 'mm', 'A4') #Creates the PDF
@@ -10,16 +11,26 @@ pdf.set_margins(26.1, 23.3, 26.1) #Sets the margins
 pdf.set_font('Arial', '', 30) #Sets the font and its style and size
 pdf.set_line_width(4) #Sets the width of the line
 pdf.set_draw_color(248, 172, 4) #Sets the color of the line (light orange)
-pdf.line(26.1, 23.3, 26.1, 240) #Sets the position of the line
+pdf.line(26.1, 23.3, 26.1, 245) #Sets the position of the line
 
-pdf.image('/home/devasc/labs/devnet-src/python/lab1-python-ac-ob/tecnocampus-logo.jpg', x=200, y=23.3, w=20) #Adds an image (logo)
-pdf.set_xy(55, 70) #Sets in which x and y of the page starts writting
-title = "Informació infraestructura de Xarxa TecnoCampus ("
-text = title + name + ")"
-pdf.cell(w=0, h=0, txt=text, border=0, ln=2, align='L') #Writes the title
-pdf.ln()
-date = "Febrer 2021"
-pdf.cell(w=0, h=0, txt=date, border=0, ln=2, align='L')
+pdf.image('/home/devasc/labs/devnet-src/python/lab1-python-ac-ob/tecnocampus-logo.jpg', x=130, y=23.3, w=70) #Adds an image (logo)
+pdf.set_xy(40, 70) #Sets in which x and y of the page starts writting
+title1 = "Informació infraestructura"
+title2 = "de Xarxa TecnoCampus"
+ex = "(" + name + ")"
+pdf.cell(w=0, h=0, txt=title1, border=0, ln=2, align='L') #Writes the 1st lane of the title
+pdf.ln(15)
+pdf.set_x(40)
+pdf.cell(w=0, h=0, txt=title2, border=0, ln=2, align='L') #Writes the 2nd lane of the title
+pdf.ln(15)
+pdf.set_x(40)
+pdf.cell(w=0, h=0, txt=ex, border=0, ln=2, align='L') #Writes the name of the Ex
+pdf.ln(30)
+pdf.set_x(40)
+
+dt = datetime.datetime.today()
+date = dt.strftime("%B %Y")
+pdf.cell(w=0, h=0, txt=date, border=0, ln=2, align='L') #Writes the date
 
 name = name + ".pdf"
 filename = "/home/devasc/labs/devnet-src/python/lab1-python-ac-ob/" + name
