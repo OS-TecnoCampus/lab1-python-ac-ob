0598676041f1b3368d14e164f35b9b2638d06950
NetAcad DEVASC <changeme@example.com>
2021-03-12 17:25:04 +0100

    All :D

--- pdfCreator.py
+++ pdfCreator.py
@@ -9,8 +9,10 @@ import matplotlib.pyplot as plt
 pdf = FPDF('P', 'mm', 'A4') #Creates the PDF
 name = input("Enter the file name: ") #Asks the user to enter the name of the file
 title = "Informació infraestructura de Xarxa TecnoCampus "
+indexLinks = []
+indexPages = []
 label = [] #Array to save the names of the devices
-conf = []
+conf = [] #Array to save the configuration of the devices
 
 #To get the information inside yaml file
 with open(name) as file:
@@ -115,7 +117,7 @@ def getMonthNum(month):
 #TITTLE PAGE
 pdf.add_page() #Adds a new page
 title_page()
-#INDEX
+#INDEX PAGE
 pdf.add_page() #Adds a new page
 header()
 footer()
@@ -128,9 +130,17 @@ pdf.set_xy(26.1, 23.3)
 pdf.set_font('Arial', '', 16) #Sets the font and its style and size
 pdf.set_text_color(46, 83, 149) #Sets the color of the text (dark blue)
 text = "1.- Introducció"
+link = pdf.add_link()
+pdf.set_link(link, pdf.get_y() , pdf.page_no())
+indexLinks.append(link)
+indexPages.append(pdf.page_no())
 pdf.cell(w=0, h=10, txt=text, border=0, ln=2, align='L')
 pdf.set_font('Arial', '', 13) #Sets the font and its style and size
 text = "1.1.- Descripció"
+link = pdf.add_link()
+pdf.set_link(link, pdf.get_y() , pdf.page_no())
+indexLinks.append(link)
+indexPages.append(pdf.page_no())
 pdf.cell(w=0, h=5, txt=text, border=0, ln=2, align='L')
 pdf.set_font('Arial', '', 11) #Sets the font and its style and size
 pdf.set_text_color(0, 0, 0) #Sets the color of the text (black)
@@ -140,6 +150,10 @@ pdf.ln(3)
 pdf.set_font('Arial', '', 13) #Sets the font and its style and size
 pdf.set_text_color(46, 83, 149) #Sets the color of the text (dark blue)
 text = "1.2.- Objectius"
+link = pdf.add_link()
+pdf.set_link(link, pdf.get_y() , pdf.page_no())
+indexLinks.append(link)
+indexPages.append(pdf.page_no())
 pdf.cell(w=0, h=5, txt=text, border=0, ln=2, align='L')
 pdf.set_font('Arial', '', 11) #Sets the font and its style and size
 pdf.set_text_color(0, 0, 0) #Sets the color of the text (black)
@@ -149,6 +163,10 @@ pdf.ln(3)
 pdf.set_font('Arial', '', 13) #Sets the font and its style and size
 pdf.set_text_color(46, 83, 149) #Sets the color of the text (dark blue)
 text = "1.3.- Descripció General de les infraestructures"
+link = pdf.add_link()
+pdf.set_link(link, pdf.get_y() , pdf.page_no())
+indexLinks.append(link)
+indexPages.append(pdf.page_no())
 pdf.cell(w=0, h=5, txt=text, border=0, ln=2, align='L')
 pdf.set_font('Arial', '', 11) #Sets the font and its style and size
 pdf.set_text_color(0, 0, 0) #Sets the color of the text (black)
@@ -180,6 +198,10 @@ pdf.ln(3)
 pdf.set_font('Arial', '', 16) #Sets the font and its style and size
 pdf.set_text_color(46, 83, 149) #Sets the color of the text (dark blue)
 text = "2.- Configuració dels dispositius"
+link = pdf.add_link()
+pdf.set_link(link, pdf.get_y() , pdf.page_no())
+indexLinks.append(link)
+indexPages.append(pdf.page_no())
 pdf.cell(w=0, h=7, txt=text, border=0, ln=2, align='L')
 pdf.set_font('Arial', '', 11) #Sets the font and its style and size
 pdf.set_text_color(0, 0, 0) #Sets the color of the text (black)
@@ -192,6 +214,10 @@ for device in range(len(nodes)):
     pdf.set_font('Arial', '', 13) #Sets the font and its style and size
     pdf.set_text_color(46, 83, 149) #Sets the color of the text (dark blue)
     text = "2." + str(device + 1) + ".- " + label[device]
+    link = pdf.add_link()
+    pdf.set_link(link, pdf.get_y() , pdf.page_no())
+    indexLinks.append(link)
+    indexPages.append(pdf.page_no())
     pdf.cell(w=0, h=5, txt=text, border=0, ln=2, align='L')
     pdf.ln(2)
     #Get configuration
@@ -208,6 +234,10 @@ for device in range(len(nodes)):
         pdf.set_font('Arial', '', 13) #Sets the font and its style and size
         pdf.set_text_color(46, 83, 149) #Sets the color of the text (dark blue)
         text = "2." + str(device + 1) + "." + str(partCounter) + ".- Configuració criptogràfica del dispositiu"
+        link = pdf.add_link()
+        pdf.set_link(link, pdf.get_y() , pdf.page_no())
+        indexLinks.append(link)
+        indexPages.append(pdf.page_no())
         partCounter+=1
         pdf.cell(w=0, h=5, txt=text, border=0, ln=2, align='L')
         pdf.set_font('Arial', '', 11) #Sets the font and its style and size
@@ -247,6 +277,10 @@ for device in range(len(nodes)):
         pdf.set_font('Arial', '', 13) #Sets the font and its style and size
         pdf.set_text_color(46, 83, 149) #Sets the color of the text (dark blue)
         text = "2." + str(device + 1) + "." + str(partCounter) + ".- Interfícies"
+        link = pdf.add_link()
+        pdf.set_link(link, pdf.get_y() , pdf.page_no())
+        indexLinks.append(link)
+        indexPages.append(pdf.page_no())
         partCounter+=1
         pdf.cell(w=0, h=5, txt=text, border=0, ln=2, align='L')
         pdf.set_font('Arial', '', 11) #Sets the font and its style and size
@@ -258,6 +292,10 @@ for device in range(len(nodes)):
         pdf.set_font('Arial', '', 13) #Sets the font and its style and size
         pdf.set_text_color(46, 83, 149) #Sets the color of the text (dark blue)
         text = "2." + str(device + 1) + "." + str(partCounter) + ".-  Configuració dels protocols d’enrutament"
+        link = pdf.add_link()
+        pdf.set_link(link, pdf.get_y() , pdf.page_no())
+        indexLinks.append(link)
+        indexPages.append(pdf.page_no())
         partCounter+=1
         pdf.cell(w=0, h=5, txt=text.replace(u"\u2019", "'"), border=0, ln=2, align='L')
         pdf.set_font('Arial', '', 11) #Sets the font and its style and size
@@ -280,11 +318,14 @@ for device in range(len(nodes)):
         else:
             text = "No s’utilitza protocol d’enrutament."
             pdf.multi_cell(w=0, h=5, txt=text.replace(u"\u2019", "'"), border=0, align='J', fill=False)
-            pdf.ln(3)
         pdf.ln(3)
         pdf.set_font('Arial', '', 13) #Sets the font and its style and size
         pdf.set_text_color(46, 83, 149) #Sets the color of the text (dark blue)
         text = "2." + str(device + 1) + "." + str(partCounter) + ".-  Configuració de Llistes de Control d’Accés"
+        link = pdf.add_link()
+        pdf.set_link(link, pdf.get_y() , pdf.page_no())
+        indexLinks.append(link)
+        indexPages.append(pdf.page_no())
         partCounter+=1
         pdf.cell(w=0, h=5, txt=text.replace(u"\u2019", "'"), border=0, ln=2, align='L')
         pdf.set_font('Arial', '', 11) #Sets the font and its style and size
@@ -311,6 +352,10 @@ for device in range(len(nodes)):
         pdf.set_font('Arial', '', 13) #Sets the font and its style and size
         pdf.set_text_color(46, 83, 149) #Sets the color of the text (dark blue)
         text = "2." + str(device + 1) + "." + str(partCounter) + ".-  Configuració de Banners"
+        link = pdf.add_link()
+        pdf.set_link(link, pdf.get_y() , pdf.page_no())
+        indexLinks.append(link)
+        indexPages.append(pdf.page_no())
         partCounter+=1
         pdf.cell(w=0, h=5, txt=text, border=0, ln=2, align='L')
         pdf.set_font('Arial', '', 11) #Sets the font and its style and size
@@ -324,6 +369,10 @@ for device in range(len(nodes)):
         pdf.set_font('Arial', '', 13) #Sets the font and its style and size
         pdf.set_text_color(46, 83, 149) #Sets the color of the text (dark blue)
         text = "2." + str(device + 1) + "." + str(partCounter) + ".- Interfícies"
+        link = pdf.add_link()
+        pdf.set_link(link, pdf.get_y() , pdf.page_no())
+        indexLinks.append(link)
+        indexPages.append(pdf.page_no())
         partCounter+=1
         pdf.cell(w=0, h=5, txt=text, border=0, ln=2, align='L')
         pdf.set_font('Arial', '', 11) #Sets the font and its style and size
@@ -343,6 +392,10 @@ for device in range(len(nodes)):
 pdf.set_font('Arial', '', 16) #Sets the font and its style and size
 pdf.set_text_color(46, 83, 149) #Sets the color of the text (dark blue)
 text = "3.- Interfícies"
+link = pdf.add_link()
+pdf.set_link(link, pdf.get_y() , pdf.page_no())
+indexLinks.append(link)
+indexPages.append(pdf.page_no())
 pdf.cell(w=0, h=5, txt=text, border=0, ln=2, align='L')
 pdf.set_font('Arial', '', 11) #Sets the font and its style and size
 pdf.set_text_color(0, 0, 0) #Sets the color of the text (black)
@@ -361,6 +414,120 @@ for x in links:
     pdf.set_font('Arial', '', 11) #Sets the font and its style and size
     text = "                    connecta " + x["i1"] + " (" + label[int(n1)-1] + ") amb " + x["i2"] + " (" + label[int(n2)-1] + ")"
     pdf.cell(w=0, h=0, txt=text.replace(u"\u2019", "'"), ln=2, align='L')
+#Interfaces Table
+pdf.ln(-10)
+epw = pdf.w - 2*pdf.l_margin
+col_width = epw/7
+data = [['Xarxa','Equip1','Interfície1','IP1','Equip2','Interfície2','IP2'],
+['J10.1.1.0','iosv2','i1','10.1.1.1','iosv1','i2','10.1.1.2'],
+['10.2.2.0','iosv2','i3','10.2.2.1','iosv3','i1','10.2.2.2'],[
+'192.168.1.0','iosv1','i1','192.168.1.1','alpine-a','i0','192.168.1.3'],[
+'192.168.2.0','iosv2','i2','192.168.2.1','alpine-b','i0','192.168.2.3'],[
+'192.168.3.0','iosv3','i2','192.168.3.1','alpine-c','i0','192.168.3.3']]
+th = pdf.font_size # Text height is the same as current font size
+pdf.ln(4*th) # Line break equivalent to 4 lines
+pdf.set_font('Arial','B',14.0) 
+pdf.cell(epw, 0.0,border=0, align='C')
+pdf.set_font('Arial','',10.0) 
+pdf.ln(0.5)
+pdf.set_draw_color(0,0,0) 
+#Here we add more padding by passing 2*th as height
+for row in data:
+    for datum in row:
+        # Enter data in colums
+        pdf.set_line_width(1)
+        pdf.cell(col_width, 2*th, str(datum), border=1)
+    pdf.ln(2*th)
+#INDEX
+nPage = pdf.page_no() #Returns the current page number
+pdf.page = 2 #Go to page (x)
+pdf.set_margins(26.1, 23.3, 26.1) #Sets the margins
+pdf.set_xy(26.1, 23.3)
+pdf.set_font('Arial', '', 11) #Sets the font and its style and size
+pdf.set_text_color(0, 0, 0) #Sets the color of the text (black)
+sep='   '
+text = "1.- Introducció.........................................................................................................................." 
+pdf.cell(w=0, h=0, txt=text, border=0, ln=0, align='L', link = indexLinks[0])
+text = str(indexPages[0])
+pdf.cell(w=0, h=0, txt=text, border=0, ln=2, align='L')
+pdf.ln(5)
+text = sep+"1.1.- Descripció....................................................................................................................." 
+pdf.cell(w=0, h=0, txt=text, border=0, ln=0, align='L', link = indexLinks[1])
+text = str(indexPages[1])
+pdf.cell(w=0, h=0, txt=text, border=0, ln=2, align='L')
+pdf.ln(5)
+text = sep+"1.2.- Objectius......................................................................................................................." 
+pdf.cell(w=0, h=0, txt=text, border=0, ln=0, align='L', link = indexLinks[2])
+text = str(indexPages[2])
+pdf.cell(w=0, h=0, txt=text, border=0, ln=2, align='L')
+pdf.ln(5)
+text = sep+"1.3.- Descripció General de les infraestructures..................................................................."
+pdf.cell(w=0, h=0, txt=text, border=0, ln=0, align='L', link = indexLinks[3])
+text = str(indexPages[3])
+pdf.cell(w=0, h=0, txt=text, border=0, ln=2, align='L')
+pdf.ln(5)
+text = "2.- Configuració dels dispositius.............................................................................................."
+pdf.cell(w=0, h=0, txt=text, border=0, ln=0, align='L', link = indexLinks[4])
+text = str(indexPages[4])
+pdf.cell(w=0, h=0, txt=text, border=0, ln=2, align='L')
+pdf.ln(5)
+count = 1
+count2 = 1
+linkCount = 5
+for x in nodes:
+    text = sep + '2.' + str(count) + ".-" + x["label"] +"....................................................................................................."
+    pdf.cell(w=0, h=0, txt=text, border=0, ln=0, align='L', link = indexLinks[linkCount])
+    text = str(indexPages[linkCount])
+    pdf.cell(w=0, h=0, txt=text, border=0, ln=2, align='L')
+    pdf.ln(5)
+    count2=1
+    linkCount+=1
+    txt = x["label"]
+    if txt[0]=='i':
+        text = sep+sep+'2.'+str(count)+"."+str(count2)+".-Configuració criptogràfica del dispositiu.................................................................."
+        pdf.cell(w=0, h=0, txt=text, border=0, ln=0, align='L', link = indexLinks[linkCount])
+        text = str(indexPages[linkCount])
+        pdf.cell(w=0, h=0, txt=text, border=0, ln=2, align='L')
+        pdf.ln(5)
+        count2+=1
+        linkCount+=1
+    if x["interfaces"]!="":
+        text = sep+sep+'2.'+str(count)+"."+str(count2)+".-Interfícies................................................................................................................."
+        pdf.cell(w=0, h=0, txt=text, border=0, ln=0, align='L', link = indexLinks[linkCount])
+        text = str(indexPages[linkCount])
+        pdf.cell(w=0, h=0, txt=text, border=0, ln=2, align='L')
+        pdf.ln(5)
+        count2+=1
+        linkCount+=1
+    if txt[0]=='i':
+        text = sep+sep+'2.'+str(count)+"."+str(count2)+".-Configuració dels protocols d'enrutament..............................................................."
+        pdf.cell(w=0, h=0, txt=text, border=0, ln=0, align='L', link = indexLinks[linkCount])
+        text = str(indexPages[linkCount])
+        pdf.cell(w=0, h=0, txt=text, border=0, ln=2, align='L')
+        pdf.ln(5)
+        count2+=1
+        linkCount+=1
+        text = sep+sep+'2.'+str(count)+"."+str(count2)+".-Configuració de Llistes de Control d'Accés............................................................."
+        pdf.cell(w=0, h=0, txt=text, border=0, ln=0, align='L', link = indexLinks[linkCount])
+        text = str(indexPages[linkCount])
+        pdf.cell(w=0, h=0, txt=text, border=0, ln=2, align='L')
+        pdf.ln(5)
+        count2+=1
+        linkCount+=1
+        text = sep+sep+'2.'+str(count)+"."+str(count2)+".-Configuració de Banners........................................................................................."
+        pdf.cell(w=0, h=0, txt=text, border=0, ln=0, align='L', link = indexLinks[linkCount])
+        text = str(indexPages[linkCount])
+        pdf.cell(w=0, h=0, txt=text, border=0, ln=2, align='L')
+        pdf.ln(5)
+        count2+=1
+        linkCount+=1
+    count+=1
+text ="3.- Interfícies............................................................................................................................"
+pdf.cell(w=0, h=0, txt=text, border=0, ln=0, align='L', link = indexLinks[linkCount])
+text = str(indexPages[linkCount])
+pdf.cell(w=0, h=0, txt=text, border=0, ln=2, align='L')
+pdf.ln(5)
+pdf.page = nPage #Go to page (x)
 
 name = name + ".pdf"
 pdf.output(name, 'F') #Creates the pdf file
