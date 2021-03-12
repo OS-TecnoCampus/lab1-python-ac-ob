7c75bf9202a26134cd208a4e85b75e111e58202e
NetAcad DEVASC <changeme@example.com>
2021-03-12 16:24:51 +0100

    2. all but enr, acl, banners

--- pdfCreator.py
+++ pdfCreator.py
@@ -10,6 +10,7 @@ pdf = FPDF('P', 'mm', 'A4') #Creates the PDF
 name = input("Enter the file name: ") #Asks the user to enter the name of the file
 title = "Informació infraestructura de Xarxa TecnoCampus "
 label = [] #Array to save the names of the devices
+conf = []
 
 #To get the information inside yaml file
 with open(name) as file:
@@ -18,6 +19,7 @@ with open(name) as file:
     for x in nodes:
         x["id"] #Counts number of nodes
         label.append(x["label"]) #Saves the names of the devices
+        conf.append(x["configuration"])
     
     links = f["links"]
     for x in links:
@@ -25,8 +27,8 @@ with open(name) as file:
            
 print(len(nodes))
 print(len(label))
-#for x in label:
-#    print(x)
+string = conf[0]
+print(string)
 
 #First page (Title page)
 def title_page():
@@ -72,14 +74,62 @@ def footer():
     page = str(pdf.page_no()) #Gets the page number
     pdf.cell(w=0, h=0, txt=page, border=0, ln=0, align='L') #Adds a page number
 
-#Tittle Page
+#Function to get the interfaces
+def getInterfaces(device):
+    interfaces = [] #Array to save the interfaces configuration
+    id = [] #Array to save the links id from the interfaces
+    lab = [] #Array to save the links label from the interfaces
+    interfaces = (nodes[device]["interfaces"])
+    for x in interfaces:
+        string = str(x)
+        posId = string.find("i") + 6
+        id.append(string[posId:posId+2])
+        posLabel = string.find("label") + 9
+        endLabel = string.find("', 'type'")
+        lab.append(string[posLabel:endLabel])
+    for x in range(len(id)):
+        i = x
+        text = "   -  Link " + id[i] + ": " + lab[i] + "\n"
+        pdf.multi_cell(w=0, h=5, txt=text, border=0, align='J', fill=False)
+
+#Function to get the full name of the month
+def getMonthNum(month):
+    if month == "Jan":
+        return "Gener"
+    if month == "Feb":
+        return "Febrer"
+    if month == "Mar":
+        return "Març"
+    if month == "Apr":
+        return "Abril"
+    if month == "May":
+        return "Maig"
+    if month == "Jun":
+        return "Juny"
+    if month == "Jul":
+        return "Juliol"
+    if month == "Aug":
+        return "Agost"
+    if month == "Sep":
+        return "Setembre"
+    if month == "Sept":
+        return "Setembre"
+    if month == "Oct":
+        return "Octubre"
+    if month == "Nov":
+        return "Novembre"
+    if month == "Dec":
+        return "Desembre"
+    return 0
+
+#TITTLE PAGE
 pdf.add_page() #Adds a new page
 title_page()
-#Index
+#INDEX
 pdf.add_page() #Adds a new page
 header()
 footer()
-#Introduction
+#INTRODUCTION
 pdf.add_page() #Adds a new page
 header()
 footer()
@@ -119,8 +169,7 @@ pdf.set_text_color(0, 0, 0) #Sets the color of the text (black)
 text = "Actualment la topologia té la següent distribució:"
 pdf.multi_cell(w=0, h=5, txt=text, border=0, align='J', fill=False)
 pdf.ln(3)
-
-#Generate graph
+#Generates the graph
 G = nx.DiGraph()
 for x in links:
     x["id"] #Counts number of links
@@ -136,13 +185,12 @@ nx.draw_networkx_labels(G, pos)
 plt.subplots_adjust(top=1, bottom=0.1)
 plt.savefig("graph.png")
 pdf.image('graph.png', x=60, w=85)
-
 pdf.set_font('Arial', '', 11) #Sets the font and its style and size
 pdf.set_text_color(0, 0, 0) #Sets the color of the text (black)
 text = "En aquesta topologia tenim " + str(len(nodes)) + " equips, connectats a través de " + str(len(links)) + " links."
 pdf.multi_cell(w=0, h=5, txt=text, border=0, align='J', fill=False)
 pdf.ln(7)
-#Devices Configuration
+#DEVICES CONFIGURATION
 pdf.set_font('Arial', '', 16) #Sets the font and its style and size
 pdf.set_text_color(46, 83, 149) #Sets the color of the text (dark blue)
 text = "2.- Configuració dels dispositius"
@@ -153,46 +201,91 @@ pdf.set_text_color(0, 0, 0) #Sets the color of the text (black)
 text = "A continuació, es detalla la configuració dels diferents dispositius:"
 pdf.multi_cell(w=0, h=5, txt=text, border=0, align='J', fill=False)
 pdf.ln(5)
-device = 0 #Device counter to get the correct configuration
-pdf.set_font('Arial', '', 13) #Sets the font and its style and size
-pdf.set_text_color(46, 83, 149) #Sets the color of the text (dark blue)
-text = "2.1.- " + label[device]
-pdf.cell(w=0, h=0, txt=text, border=0, ln=2, align='L')
-pdf.ln(7)
-text = "2.1.1- Interfícies"
-pdf.cell(w=0, h=0, txt=text, border=0, ln=2, align='L')
-pdf.ln(5)
-pdf.set_font('Arial', '', 11) #Sets the font and its style and size
-pdf.set_text_color(0, 0, 0) #Sets the color of the text (black)
-text = "Les interfícies i la seva configuració és:\n"
-pdf.multi_cell(w=0, h=5, txt=text, border=0, align='J', fill=False)
-interfaces = [] #Array to save the interfaces configuration
-id = [] #Array to save the links id from the interfaces
-lab = [] #Array to save the links label from the interfaces
-interfaces = (nodes[device]["interfaces"])
-for x in interfaces:
-    string = str(x)
-    posId = string.find("i") + 6
-    id.append(string[posId:posId+2])
-    posLabel = string.find("label") + 9
-    endLabel = string.find("', 'type'")
-    lab.append(string[posLabel:endLabel])
-for x in range(len(id)):
-    i = x
-    text = "   - Link " + id[i] + ": " + lab[i] + "\n"
-    pdf.multi_cell(w=0, h=5, txt=text, border=0, align='J', fill=False)
-device+=1
-pdf.ln(5)
-pdf.set_font('Arial', '', 13) #Sets the font and its style and size
-pdf.set_text_color(46, 83, 149) #Sets the color of the text (dark blue)
-text = "2.2.- " + label[device]
-pdf.cell(w=0, h=0, txt=text, border=0, ln=2, align='L')
-pdf.ln(5)
-pdf.set_font('Arial', '', 11) #Sets the font and its style and size
-pdf.set_text_color(0, 0, 0) #Sets the color of the text (black)
-text = "El darrer canvi de la configuració va ser el "
-pdf.multi_cell(w=0, h=5, txt=text, border=0, align='J', fill=False)
-pdf.ln(5)
+for device in range(len(nodes)):
+    partCounter = 1
+    string = conf[device]
+    pdf.set_font('Arial', '', 13) #Sets the font and its style and size
+    pdf.set_text_color(46, 83, 149) #Sets the color of the text (dark blue)
+    text = "2." + str(device + 1) + ".- " + label[device]
+    pdf.cell(w=0, h=0, txt=text, border=0, ln=2, align='L')
+    pdf.ln(7)
+    #Get configuration
+    if "configuration change" in string: #If the device has configuration
+        pdf.ln(-2)
+        posChange = string.find("change") + 10
+        endChange = string.find("\n!\nversion")
+        changeDate = string[posChange:endChange]
+        pdf.set_font('Arial', '', 11) #Sets the font and its style and size
+        pdf.set_text_color(0, 0, 0) #Sets the color of the text (black)
+        print(changeDate)
+        text = "El darrer canvi de la configuració va ser el " + changeDate[24:28] + " de " + getMonthNum(changeDate[17:20]) + " del " + changeDate[21:23] + " a les " + changeDate[0:12] + "."
+        pdf.multi_cell(w=0, h=5, txt=text, border=0, align='J', fill=False)
+        pdf.ln(5)
+        pdf.set_font('Arial', '', 13) #Sets the font and its style and size
+        pdf.set_text_color(46, 83, 149) #Sets the color of the text (dark blue)
+        text = "2." + str(device + 1) + "." + str(partCounter) + "- Configuració criptogràfica del dispositiu"
+        partCounter+=1
+        pdf.cell(w=0, h=0, txt=text, border=0, ln=2, align='L')
+        pdf.ln(5)
+        pdf.set_font('Arial', '', 11) #Sets the font and its style and size
+        pdf.set_text_color(0, 0, 0) #Sets the color of the text (black)
+        if "crypto" in string: #If the device has crypto configuration
+            text = "El dispositiu té la següent configuració de crypto:\n"
+            pdf.multi_cell(w=0, h=5, txt=text, border=0, align='J', fill=False)
+            text = "   -  Connexió amb " + string[string.find("address") + 8:string.find("\n!\n!\ncrypto ipsec")] + ":"
+            pdf.multi_cell(w=0, h=5, txt=text, border=0, align='J', fill=False)
+            text = "          o  Política de regles número " + string[string.find("policy") + 7:string.find("\n encr")] + ":"
+            pdf.multi_cell(w=0, h=5, txt=text, border=0, align='J', fill=False)
+            text = "                    >  Encriptació " + string[string.find("encr ") + 5:string.find("encr ") + 8] + ", " + string[string.find("encr ") + 9:string.find("\n authentication")]
+            pdf.multi_cell(w=0, h=5, txt=text, border=0, align='J', fill=False)
+            text = "                    >  Autenticació " + string[string.find("\n authentication") + 17:string.find("\n group")]
+            pdf.multi_cell(w=0, h=5, txt=text, border=0, align='J', fill=False)
+            text = "                    >  Diffie-Helmann grup " + string[string.find("\n group") + 8:string.find("\ncrypto isakmp key")]
+            pdf.multi_cell(w=0, h=5, txt=text, border=0, align='J', fill=False)
+            text = "          o  Contrasenya ISAKMP: " + string[string.find("\ncrypto isakmp key") + 18:string.find("address")] + "\n"
+            pdf.multi_cell(w=0, h=5, txt=text, border=0, align='J', fill=False)
+            transformInfo = string[string.find("transform-set") + 14:string.find("\n mode ")]
+            text = "          o  Configuració VPN:\n                    >  Conjunt de transformació " + transformInfo[0:transformInfo.find(" ")]
+            pdf.multi_cell(w=0, h=5, txt=text, border=0, align='J', fill=False)
+            transformInfo = transformInfo[transformInfo.find(" ")+1:transformInfo.find("\n")]
+            text = "                    >  Configuració Encriptació ESP: " + transformInfo[0:transformInfo.find(" ")]
+            pdf.multi_cell(w=0, h=5, txt=text, border=0, align='J', fill=False)
+            transformInfo = transformInfo[transformInfo.find(" ")+1:transformInfo.find("\n")]
+            text = "                    >  Configuració Signatura ESP: " + transformInfo[0:transformInfo.find(" ")]
+            pdf.multi_cell(w=0, h=5, txt=text, border=0, align='J', fill=False)
+            text = "                    >  Mode " + string[string.find("\n mode ") + 7:string.find("\n!\n!\n!\ncrypto map")]
+            pdf.multi_cell(w=0, h=5, txt=text, border=0, align='J', fill=False)
+            text = "                    >  ACL número " + string[string.find("match address") + 14:string.find("\n!\n!\n!\n!\n!\ninterface")]
+            pdf.multi_cell(w=0, h=5, txt=text, border=0, align='J', fill=False)
+        else: #If the device does not have crypto configuration
+            text = "El dispositiu no té configuració crypto.\n"
+            pdf.multi_cell(w=0, h=5, txt=text, border=0, align='J', fill=False)
+        pdf.ln(5)
+        pdf.set_font('Arial', '', 13) #Sets the font and its style and size
+        pdf.set_text_color(46, 83, 149) #Sets the color of the text (dark blue)
+        text = "2." + str(device + 1) + "." + str(partCounter) + "- Interfícies"
+        partCounter+=1
+        pdf.cell(w=0, h=0, txt=text, border=0, ln=2, align='L')
+        pdf.ln(5)
+        pdf.set_font('Arial', '', 11) #Sets the font and its style and size
+        pdf.set_text_color(0, 0, 0) #Sets the color of the text (black)
+        text = "Les interfícies i la seva configuració és:\n"
+        pdf.multi_cell(w=0, h=5, txt=text, border=0, align='J', fill=False)
+        getInterfaces(device)
+        pdf.ln(5)
+    else: #If the device does not have configuration
+        pdf.set_font('Arial', '', 13) #Sets the font and its style and size
+        pdf.set_text_color(46, 83, 149) #Sets the color of the text (dark blue)
+        text = "2." + str(device + 1) + "." + str(partCounter) + "- Interfícies"
+        partCounter+=1
+        pdf.cell(w=0, h=0, txt=text, border=0, ln=2, align='L')
+        pdf.ln(5)
+        pdf.set_font('Arial', '', 11) #Sets the font and its style and size
+        pdf.set_text_color(0, 0, 0) #Sets the color of the text (black)
+        text = "Les interfícies i la seva configuració és:\n"
+        pdf.multi_cell(w=0, h=5, txt=text, border=0, align='J', fill=False)
+        getInterfaces(device)
+        pdf.ln(5)
 
 pdf.add_page() #Adds a new page
 header()
